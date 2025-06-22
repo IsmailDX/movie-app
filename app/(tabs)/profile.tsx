3f922dee916fc332ce006471167ba289a9006604
ismaildx <ismaildxx@gmail.com>
2025-06-22 19:00:42 +0400

    finished app

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,14 +1,16 @@
+import { icons } from "@/constants/icons";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 
 const profile = () => {
   return (
-    <View>
-      <Text>profile</Text>
+    <View className="flex-1 bg-primary px-10">
+      <View className="flex justify-center items-center flex-1 flex-col gap-5">
+        <Image source={icons.person} className="size-10" tintColor="#fff" />
+        <Text className="text-gray-500">Profile</Text>
+      </View>
     </View>
   );
 };
 
 export default profile;
-
-const styles = StyleSheet.create({});
--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,14 +1,16 @@
+import { icons } from "@/constants/icons";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 
-const saved = () => {
+const Saved = () => {
   return (
-    <View>
-      <Text>saved</Text>
+    <View className="flex-1 bg-primary px-10">
+      <View className="flex justify-center items-center flex-1 flex-col gap-5">
+        <Image source={icons.save} className="size-10" tintColor="#fff" />
+        <Text className="text-gray-500">Saved</Text>
+      </View>
     </View>
   );
 };
 
-export default saved;
-
-const styles = StyleSheet.create({});
+export default Saved;
